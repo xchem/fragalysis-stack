e6b823116561084ae38bf3c1412c80aaa4d7b9fe
abradle <anthony.richard.bradley@gmail.com>
2018-01-27 08:11:04 +0000

    add

---
+++ run_rhel.sh
@@ -0,0 +1,18 @@
+npm install
+python manage.py migrate        # Apply database migrations
+python manage.py collectstatic --clear --noinput # clearstatic files
+python manage.py collectstatic --noinput  # collect static files
+# Prepare log files and start outputting logs to stdout
+touch /srv/logs/gunicorn.log
+touch /srv/logs/access.log
+tail -n 0 -f /srv/logs/*.log &
+# Start Gunicorn processes
+echo Starting Gunicorn.
+exec gunicorn fragalysis.wsgi:application \
+    --name fragalysis \
+    --bind unix:django_app.sock \
+    --workers 3 \
+    --log-level=info \
+    --log-file=/srv/logs/gunicorn.log \
+    --access-logfile=/srv/logs/access.log &
+sudo service nginx start
