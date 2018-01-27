c1bdf763f0b4f17120e370350173839773c4014d
abradle <anthony.richard.bradley@gmail.com>
2018-01-27 07:13:27 +0000

    Setup for rhel7

--- fragalysis/settings.py
+++ fragalysis/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': 5432,
     }
 }
