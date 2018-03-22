8854c9fba7ff7a323c9c47f8dd482bffd28a5b3e
Alan Christie <alan.christie@matildapeak.com>
2018-03-22 15:20:04 +0000

    - Changes to let latest web app start
    - Replicates what was done in the repo's previous incarnation.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+# Wait for media
+date
+echo "Waiting for media..."
+until [ -f /code/media/loaded ]
+do
+    sleep 2
+done
+date
+echo "Media is ready."
+
+echo "Running migrations..."
 cd /code
 python /code/manage.py makemigrations auth
 python /code/manage.py migrate auth
@@ -6,23 +17,49 @@ python /code/manage.py makemigrations scoring
 python /code/manage.py migrate scoring
 python /code/manage.py  makemigrations
 python /code/manage.py  migrate     # Apply database migrations - weird order is due to https://stackoverflow.com/questions/31417470/django-db-utils-programmingerror-relation-app-user-does-not-exist-during-ma
+echo "Running loader..."
 python loader.py
+echo "Running collectstatic..."
 python manage.py collectstatic --clear --noinput -v 0 # clearstatic files
 python manage.py collectstatic --noinput -v 0 # collect static files
+
+echo "Creating superuser..."
+
+# Automatically create the superuser...
+script="
+from django.contrib.auth.models import User;
+
+username = '$WEB_DJANGO_SUPERUSER_NAME';
+password = '$WEB_DJANGO_SUPERUSER_PASSWORD';
+email = '$WEB_DJANGO_SUPERUSER_EMAIL';
+
+if User.objects.filter(username=username).count()==0:
+    User.objects.create_superuser(username, email, password);
+    print('Superuser created.');
+else:
+    print('Superuser creation skipped.');
+"
+printf "$script" | python manage.py shell
+
 # Prepare log files and start outputting logs to stdout
 touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
 tail -n 0 -f /srv/logs/*.log &
 # Start the NPM build
-echo Starting
+echo "Starting..."
 cd /code/frontend && npm run dev &
 # Start Gunicorn processes
-echo Starting Gunicorn.
-exec gunicorn fragalysis.wsgi:application \
+echo "Starting Gunicorn...."
+gunicorn fragalysis.wsgi:application \
+    --daemon \
     --name fragalysis \
     --bind unix:django_app.sock \
     --workers 3 \
-    --log-level=info \
+    --log-level=debug \
     --log-file=/srv/logs/gunicorn.log \
-    --access-logfile=/srv/logs/access.log &
-exec service nginx start
+    --access-logfile=/srv/logs/access.log
+
+echo "Testing nginx config..."
+nginx -t
+echo "Running nginx..."
+nginx
